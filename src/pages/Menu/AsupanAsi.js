9459ece6b9adcd4d8e5c48bf5df743c4fbdcc37c
Fachreza Maulana <zzafachreza@gmail.com>
2023-12-31 04:22:25 +0700

    progress 231231

--- src/pages/Menu/AsupanAsi.js
+++ src/pages/Menu/AsupanAsi.js
@@ -6,10 +6,17 @@ import YoutubePlayer from "react-native-youtube-iframe";
 import axios from 'axios';
 import { apiURL } from '../../utils/localStorage';
 import moment from 'moment';
-import { MyHeader } from '../../components';
-
+import { showMessage } from 'react-native-flash-message';
+import { MyButton, MyCalendar, MyGap, MyHeader, MyInput, MyPicker, MyRadio } from '../../components';
+import { ScrollView } from 'react-native';
 export default function AsupanAsi({ navigation, route }) {
     const [loading, setLoading] = useState(false);
+    const [kirim, setKirim] = useState({
+        tanggal: moment().format('YYYY-MM-DD'),
+        waktu_pemberian: '',
+        frek_menyusui: 1,
+        durasi_menyusui: ''
+    })
     return (
         <SafeAreaView style={{
             flex: 1,
@@ -18,7 +25,132 @@ export default function AsupanAsi({ navigation, route }) {
 
             <MyHeader judul="Asupan ASI" onPress={() => navigation.goBack()} />
 
+            <View style={{
+                padding: 10,
+                borderWidth: 1,
+                margin: 20,
+                borderRadius: 10,
+                borderColor: colors.border
+            }}>
+                <MyCalendar value={kirim.tanggal} onDateChange={x => {
+                    setKirim({
+                        ...kirim,
+                        tanggal: x
+                    })
+                }} label="Tanggal Pengukuran" iconname="calendar-outline" />
+                <MyGap jarak={10} />
+                <MyPicker iconname="time-outline" data={[
+                    { label: 'Pagi', value: 'Pagi' },
+                    { label: 'Siang', value: 'Siang' },
+                    { label: 'Sore', value: 'Sore' },
+                    { label: 'Malam', value: 'Malam' },
+                ]} label="Waktu Pemberian" value={kirim.waktu_pemberian} onValueChange={x => {
+                    setKirim({
+                        ...kirim,
+                        waktu_pemberian: x
+                    })
 
+                }} />
+                <MyGap jarak={10} />
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingVertical: 0,
+                        position: 'relative'
+                    }}>
+                    <Icon type="ionicon" name="aperture-outline" color={colors.primary} size={MyDimensi / 4} />
+                    <Text
+                        style={{
+                            fontFamily: fonts.secondary[600],
+                            color: colors.primary,
+                            left: 10,
+                            fontSize: MyDimensi / 4,
+                        }}>
+                        Frekuensi Menyusui
+                    </Text>
+                </View>
+                <View style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginVertical: 10,
+                }}>
+
+
+                    <TouchableWithoutFeedback onPress={() => {
+                        if (kirim.frek_menyusui == 1) {
+                            showMessage({
+                                message: 'Minimal Frekuensi Pemberian Makanan Utama 1',
+                                type: 'danger'
+                            })
+                        } else {
+                            setKirim({
+                                ...kirim,
+                                frek_menyusui: kirim.frek_menyusui - 1
+                            });
+
+                        }
+                    }}>
+                        <View style={{
+                            flex: 1,
+                            justifyContent: 'flex-end',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                        }}>
+                            <View style={{
+                                width: MyDimensi / 1.5,
+                                height: MyDimensi / 1.5,
+                                backgroundColor: colors.primary,
+                                borderRadius: MyDimensi / 3,
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <Icon type='ionicon' name='remove' color={colors.white} />
+                            </View>
+                        </View>
+                    </TouchableWithoutFeedback>
+                    <Text style={{
+                        flex: 1,
+                        fontFamily: fonts.secondary[800],
+                        fontSize: MyDimensi / 2.5,
+                        textAlign: 'center',
+                    }}>{kirim.frek_menyusui}</Text>
+                    <TouchableWithoutFeedback onPress={() => {
+                        setKirim({
+                            ...kirim,
+                            frek_menyusui: kirim.frek_menyusui + 1
+                        });
+
+                    }}>
+                        <View style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}>
+                            <View style={{
+                                width: MyDimensi / 1.5,
+                                height: MyDimensi / 1.5,
+                                backgroundColor: colors.primary,
+                                borderRadius: MyDimensi / 3,
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <Icon type='ionicon' name='add' color={colors.white} />
+                            </View>
+                        </View>
+                    </TouchableWithoutFeedback>
+                </View>
+                <MyGap jarak={10} />
+
+                <MyInput label="Durasi Menysui ( Menit )" keyboardType="number-pad" onChangeText={x => {
+                    setKirim({
+                        ...kirim,
+                        durasi_menyusui: x
+                    })
+                }} />
+                <MyGap jarak={30} />
+                <MyButton title="Kirim" />
+            </View>
 
 
             {loading &&
--- src/pages/Menu/StatusGizi.js
+++ src/pages/Menu/StatusGizi.js
@@ -6,10 +6,16 @@ import YoutubePlayer from "react-native-youtube-iframe";
 import axios from 'axios';
 import { apiURL } from '../../utils/localStorage';
 import moment from 'moment';
-import { MyHeader } from '../../components';
-
+import { showMessage } from 'react-native-flash-message';
+import { MyButton, MyCalendar, MyGap, MyHeader, MyInput, MyPicker, MyRadio } from '../../components';
+import { ScrollView } from 'react-native';
 export default function StatusGizi({ navigation, route }) {
     const [loading, setLoading] = useState(false);
+    const [kirim, setKirim] = useState({
+        tanggal: moment().format('YYYY-MM-DD'),
+        berat_badan: '',
+        tinggi_badan: '',
+    })
     return (
         <SafeAreaView style={{
             flex: 1,
@@ -18,7 +24,40 @@ export default function StatusGizi({ navigation, route }) {
 
             <MyHeader judul="Status Gizi" onPress={() => navigation.goBack()} />
 
+            <View style={{
+                padding: 10,
+                borderWidth: 1,
+                margin: 20,
+                borderRadius: 10,
+                borderColor: colors.border
+            }}>
+                <MyCalendar value={kirim.tanggal} onDateChange={x => {
+                    setKirim({
+                        ...kirim,
+                        tanggal: x
+                    })
+                }} label="Tanggal Pengukuran" iconname="calendar-outline" />
+                <MyGap jarak={10} />
 
+
+                <MyInput label="Berat Badan Saat Ini ( kg )" keyboardType="number-pad" onChangeText={x => {
+                    setKirim({
+                        ...kirim,
+                        berat_badan: x
+                    })
+                }} />
+                <MyGap jarak={10} />
+
+
+                <MyInput label="Tinggi Badan Saat Ini ( cm )" keyboardType="number-pad" onChangeText={x => {
+                    setKirim({
+                        ...kirim,
+                        tinggi_badan: x
+                    })
+                }} />
+                <MyGap jarak={30} />
+                <MyButton title="Kirim" />
+            </View>
 
 
             {loading &&
